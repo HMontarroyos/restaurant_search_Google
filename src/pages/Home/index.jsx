90ad147ea1b149651c00624d10044d5c057edb5b
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2021-09-19 11:41:50 -0300

    feat: criando carousel com a lib react-slick e adicionando o componente ImageCard

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,39 @@
 import React, { useState } from 'react';
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
-
 import logo from '../../assets/logo.svg';
 
-import { Container, Search, Logo, Wrapper, CarouselTitle } from './styles';
+import { Container, Search, Logo, Wrapper, Carousel, CarouselTitle } from './styles';
+import { ImageCard } from '../../components/ImageCard';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
-
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true,
+  };
   return (
-      <Wrapper>
-        <Container>
+    <Wrapper>
+      <Container>
         <Search>
-            <Logo src={logo} alt="logo da empresa" />
-            <TextField
+          <Logo src={logo} alt="logo da empresa" />
+          <TextField
             outlined
             label="Pesquisar Restaurantes"
             trailingIcon={<MaterialIcon role="button" icon="search" />}>
             <Input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-            </TextField>
-            <CarouselTitle>Na sua Área</CarouselTitle>
+          </TextField>
+          <CarouselTitle>Na sua Área</CarouselTitle>
+          <Carousel {...settings}>
+            <ImageCard photo={'teste'} title={'teste'} />
+          </Carousel>
         </Search>
-        </Container>
-      </Wrapper>
+      </Container>
+    </Wrapper>
   );
 };
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Slider from 'react-slick';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -24,11 +25,17 @@ export const Logo = styled.img`
   margin-bottom: 15px;
 `;
 
+export const Carousel = styled(Slider)`
+  .slick-slide {
+    margin-right: 16px;
+  }
+`;
+
 export const CarouselTitle = styled.h1`
-    font-family: ${(props)=> props.theme.fonts.regular};
-    color: ${(props)=> props.theme.colors.text};
-    font-size: 24px;
-    font-weight:bold;
-    line-height:29px;
-    margin: 16px 0;
+  font-family: ${(props) => props.theme.fonts.regular};
+  color: ${(props) => props.theme.colors.text};
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 29px;
+  margin: 16px 0;
 `;
