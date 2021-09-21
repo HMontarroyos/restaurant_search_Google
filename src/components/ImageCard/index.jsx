7e67f85a43bdde9654208216faa698f189866e32
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2021-09-21 08:05:48 -0300

    fix: adjusting skeleton

--- src/components/ImageCard/index.jsx
+++ src/components/ImageCard/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Title } from './styles';
 
-const ImageCard = ({ photo, title }) => (
-  <Card photo={photo}>
-    <Title>{title}</Title>
-  </Card>
-);
+import Skeleton from '../Skeleton';
+
+const ImageCard = ({ photo, title }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const imageLoader = new Image();
+    imageLoader.src = photo;
+    imageLoader.onload = () => setImageLoaded(true);
+  }, [photo]);
+
+  return (
+    <>
+      {imageLoaded ? (
+        <Card photo={photo}>
+          <Title>{title}</Title>
+        </Card>
+      ) : (
+        <Skeleton width="90px" height="90px" />
+      )}
+    </>
+  );
+};
 
 export default ImageCard;
--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactStars from 'react-rating-stars-component';
 import Image from '../../assets/restaurante-fake.png';
 
 import { Restaurant, RestaurantInfo, RestaurantPhoto } from './styles';
+import Skeleton from '../Skeleton';
 
-const RestaurantCard = ({ restaurant, onClick }) => (
-  <Restaurant onClick={onClick}>
-    <RestaurantInfo>
-      <ReactStars count={5} value={restaurant.rating} edit={false} isHalf activeColor="#e7711c" />
-    </RestaurantInfo>
-    <RestaurantPhoto
-      src={restaurant.photos ? restaurant.photos[0].getUrl() : Image}
-      alt={'Foto do Restaurante'}
-    />
-  </Restaurant>
-);
+const RestaurantCard = ({ restaurant, onClick }) => {
+  const [imageLoading, setImageLoading] = useState(false);
+
+  return (
+    <Restaurant onClick={onClick}>
+      <RestaurantInfo>
+        <ReactStars count={5} value={restaurant.rating} edit={false} isHalf activeColor="#e7711c" />
+      </RestaurantInfo>
+      <RestaurantPhoto
+        imageLoaded={imageLoading}
+        src={restaurant.photos ? restaurant.photos[0].getUrl() : Image}
+        onLoad={() => setImageLoading(true)}
+        alt={'Foto do Restaurante'}
+      />
+      {!imageLoading && <Skeleton width="100px" height="100px" />}
+    </Restaurant>
+  );
+};
 
 export default RestaurantCard;
